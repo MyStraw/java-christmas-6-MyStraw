2b93e78c89b382fa277521cec4074c82ad507e46
MyStraw <kikongdosa@gmail.com>
2023-11-14 08:39:03 +0900

    Feat(Application): 간단한 입출력 기능 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,34 @@
 package christmas;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        int day = Integer.parseInt(Console.readLine());
+
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String order = Console.readLine();
+
+
+
+        System.out.println("12월 " + day + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        System.out.println("<주문 메뉴>");
+        System.out.println(order.replace(",", "\n"));
+        System.out.println("\n<할인 전 총주문 금액>");
+
+
+        // 증정 메뉴 출력
+        System.out.println("\n<증정 메뉴>");
     }
 }
+
+
+
+
