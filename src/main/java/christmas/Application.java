d2d161d93c0676ab9475508626deb6c1d02314ab
MyStraw <kikongdosa@gmail.com>
2023-11-15 23:09:56 +0900

    refactor(Application): 증정선물 판정코드 이동 및 띄어쓰기 개선

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,44 +2,41 @@ package christmas;
 
 import christmas.constant.Constants;
 import christmas.controller.BadgeController;
+import christmas.controller.DiscountController;
+import christmas.controller.MenusController;
+import christmas.controller.OrdersController;
 import christmas.model.Orders;
-import christmas.service.DayService;
-import christmas.service.DiscountService;
-import christmas.service.MenusService;
-import christmas.service.OrdersService;
 import christmas.util.StringUtil;
-import christmas.view.InputView;
 import christmas.view.OutputView;
+import christmas.view.validation.InputViewValidation;
 
 import java.time.LocalDate;
 
+
 public class Application {
     public static void main(String[] args) {
-
-        int day = InputView.getVisitDay();
-        String order = InputView.getOrder();
+        InputViewValidation validator = new InputViewValidation();
+        int day = validator.getValidVisitDay();
+        String order = validator.getValidOrder();
 
         String[] orderedItems = StringUtil.splitString(order, ",");
-        LocalDate visitDate = LocalDate.of(2023, 12, day);
+        LocalDate visitDate = LocalDate.of(Constants.YEAR, Constants.MONTH, day);
 
-        OrdersService ordersService = new OrdersService();
-        DiscountService discountService = new DiscountService();
+        OrdersController ordersController = new OrdersController();
+        DiscountController discountController = new DiscountController();
+
         BadgeController badgeController = new BadgeController();
-        Orders orderResult = ordersService.processOrder(orderedItems, visitDate, new DayService());
+        Orders orderResult = ordersController.processOrder(orderedItems, visitDate);
+        validator.validateOrder(orderResult);
         // 증정 이벤트 및 할인 계산
-        String giftedItem = determineGiftedItem(orderResult);
-        int giftedItemValue = MenusService.getChampagnePrice();
-        int totalDiscount = discountService.calculateTotalDiscount(orderResult, giftedItem, giftedItemValue);
+        String giftedItem =ordersController.determineGiftedItem(orderResult);
+        MenusController menusController = new MenusController();
+        int giftedItemValue = menusController.getChampagnePrice();
+        int totalDiscount = discountController.calculateTotalDiscount(orderResult, giftedItem, giftedItemValue);
         // 결과 출력
-        OutputView.printOrderSummary(day, orderedItems, orderResult, giftedItem, totalDiscount, discountService);
+        OutputView.printOrderSummary(day, orderedItems, orderResult, giftedItem, totalDiscount, discountController);
         // 최종 결제 금액 및 이벤트 배지 출력
         OutputView.printFinalPaymentAndBadge(orderResult, totalDiscount, giftedItemValue, badgeController);
-    }
-    private static String determineGiftedItem(Orders orders) {
-        if (orders.getTotalCost() >= Constants.CHAMPAGNE_GIFT_MIN_AMOUNT) {
-            return "샴페인 1개";
-        }
-        return "";
     }
 }
 
