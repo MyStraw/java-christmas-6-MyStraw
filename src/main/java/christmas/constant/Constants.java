ecdc1690101a6873549ba7d9812416f6fc3790e8
MyStraw <kikongdosa@gmail.com>
2023-11-15 15:39:48 +0900

    feat(Constants): 배지 상수 추가
    산타, 트리, 별에 해당하는 상수 추가

--- src/main/java/christmas/constant/Constants.java
+++ src/main/java/christmas/constant/Constants.java
@@ -1,13 +1,21 @@
 package christmas.constant;
 
 public class Constants {
-    public static final int WEEKDAY_DISCOUNT_PER_DESSERT = 2023;
-    public static final int WEEKEND_DISCOUNT_PER_MAIN_DISH = 2023;
-    public static final int SPECIAL_DISCOUNT_AMOUNT = 1000;
-    public static final int CHAMPAGNE_PRICE = 25000;
-    public static final int MIN_ORDER_AMOUNT = 10000;
+    public static final int WEEKDAY_DISCOUNT_PER_DESSERT = 2_023;
+    public static final int WEEKEND_DISCOUNT_PER_MAIN_DISH = 2_023;
+
+    public static final int DISCOUNT_PER_DAY = 100;
+    public static final int CHRISTMAS = 25;
+    public static final int SPECIAL_DISCOUNT_AMOUNT = 1_000;
+    public static final int CHAMPAGNE_PRICE = 25_000;
+    public static final int MIN_ORDER_AMOUNT = 10_000;
     public static final int MAX_ITEM_COUNT = 20;
-    public static final int CHAMPAGNE_GIFT_MIN_AMOUNT = 120000;
+    public static final int CHAMPAGNE_GIFT_MIN_AMOUNT = 120_000;
+    public static final int SANTA = 20_000;
+    public static final int TREE = 10_000;
+    public static final int STAR = 5_000;
+
+
     private Constants() { //인스턴스 생성을 애초에 막아버리기
 
     }
