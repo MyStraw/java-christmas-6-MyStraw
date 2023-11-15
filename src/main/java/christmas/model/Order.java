6830db4f4326cd4dba17b8aee6dcf96074db4dec
MyStraw <kikongdosa@gmail.com>
2023-11-15 15:20:28 +0900

    feat(Order): 해당되는 할인 판별하는 기능 추가
    평일인지 주말인지 판별후 할인적용

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -1,5 +1,6 @@
 package christmas.model;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class Order {
@@ -21,5 +22,51 @@ public class Order {
         this.weekendDiscount = weekendDiscount;
         this.ddayDiscount = ddayDiscount;
         this.specialDiscount = specialDiscount;
+    }
+
+    public static Order processOrder(String[] orderedItems, LocalDate visitDate,
+                                     Day discountCalculator) {
+        int totalCost = 0;
+        int totalItemsCount = 0;
+        boolean onlyDrink = true;
+        int weekdayDiscount = 0;
+        int weekendDiscount = 0;
+        int ddayDiscount = discountCalculator.calculateDdayDiscount(visitDate);
+        int specialDiscount = 0;
+
+        if (discountCalculator.isSpecialDay(visitDate)) {
+            specialDiscount = discountCalculator.calculateSpecialDiscount(visitDate, true);
+        }
+
+        boolean isWeekend = discountCalculator.isWeekEnd(visitDate);
+
+        for (String item : orderedItems) {
+            String[] parts = item.trim().split("-");
+            String menuItemName = parts[0];
+            int quantity = Integer.parseInt(parts[1]);
+            totalItemsCount += quantity;
+
+            for (List<Menu> menuList : List.of(Menu.getAppetizer(), Menu.getMainDishes(),
+                    Menu.getDesserts(), Menu.getDrinks())) {
+                for (Menu menuItem : menuList) {
+                    if (menuItem.getName().equals(menuItemName)) {
+                        totalCost += menuItem.getPrice() * quantity;
+                        if (!menuList.equals(Menu.getDrinks())) {
+                            onlyDrink = false;
+                        }
+
+                        if (menuList.equals(Menu.getDesserts()) && !isWeekend) {
+                            weekdayDiscount += discountCalculator.calculateWeekdayDiscount(quantity);
+                        } else if (menuList.equals(Menu.getMainDishes()) && isWeekend) {
+                            weekendDiscount += discountCalculator.calculateWeekendDiscount(quantity);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        return new Order(totalCost, totalItemsCount, onlyDrink, weekdayDiscount, weekendDiscount,
+                ddayDiscount, specialDiscount);
     }
 }
