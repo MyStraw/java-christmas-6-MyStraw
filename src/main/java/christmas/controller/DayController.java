5c9d3a92a8c4bd5a215d97f06913c93db283efbc
MyStraw <kikongdosa@gmail.com>
2023-11-15 21:42:11 +0900

    feat(DayController): 컨트롤러 추가
    -서비스와 연결짓는 콘트롤러 추가

---
+++ src/main/java/christmas/controller/DayController.java
@@ -0,0 +1,24 @@
+package christmas.controller;
+
+import christmas.service.DayService;
+import java.time.LocalDate;
+
+public class DayController {
+    private final DayService dayService;
+
+    public DayController() {
+        this.dayService = new DayService();
+    }
+
+    public boolean isWeekDay(LocalDate date) {
+        return dayService.isWeekDay(date);
+    }
+
+    public boolean isWeekEnd(LocalDate date) {
+        return dayService.isWeekEnd(date);
+    }
+
+    public boolean isSpecialDay(LocalDate date) {
+        return dayService.isSpecialDay(date);
+    }
+}
