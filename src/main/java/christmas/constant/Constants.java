dbe668938964e51cde9322b3a04f19f469fd5110
MyStraw <kikongdosa@gmail.com>
2023-11-15 15:34:02 +0900

    feat(Constants): 상수 다루는 클래스 추가
    상수 모아놓기

---
+++ src/main/java/christmas/constant/Constants.java
@@ -0,0 +1,15 @@
+package christmas.constant;
+
+public class Constants {
+    public static final int WEEKDAY_DISCOUNT_PER_DESSERT = 2023;
+    public static final int WEEKEND_DISCOUNT_PER_MAIN_DISH = 2023;
+    public static final int SPECIAL_DISCOUNT_AMOUNT = 1000;
+    public static final int CHAMPAGNE_PRICE = 25000;
+    public static final int MIN_ORDER_AMOUNT = 10000;
+    public static final int MAX_ITEM_COUNT = 20;
+    public static final int CHAMPAGNE_GIFT_MIN_AMOUNT = 120000;
+    private Constants() { //인스턴스 생성을 애초에 막아버리기
+
+    }
+
+}
