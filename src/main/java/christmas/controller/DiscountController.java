f17cb3cb07cb2e5520c243b454289f457fec03cd
MyStraw <kikongdosa@gmail.com>
2023-11-15 21:44:09 +0900

    feat(DiscountController): 할인 컨트롤러 추가
    - 서비스와 연결시키는 컨트롤러 추가

---
+++ src/main/java/christmas/controller/DiscountController.java
@@ -0,0 +1,16 @@
+package christmas.controller;
+
+import christmas.model.Orders;
+import christmas.service.DiscountService;
+
+public class DiscountController {
+    private final DiscountService discountService;
+
+    public DiscountController() {
+        this.discountService = new DiscountService();
+    }
+
+    public int calculateTotalDiscount(Orders order, String giftedItem, int giftedItemValue) {
+        return discountService.calculateTotalDiscount(order, giftedItem, giftedItemValue);
+    }
+}
