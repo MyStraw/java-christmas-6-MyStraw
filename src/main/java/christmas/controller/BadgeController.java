123360bb7244f115843cc02680f155340842267b
MyStraw <kikongdosa@gmail.com>
2023-11-15 22:02:01 +0900

    feat(BadgeController): 뱃지 컨트롤러 추가
    - 서비스 로직과 메인함수 이어주는 역할

---
+++ src/main/java/christmas/controller/BadgeController.java
@@ -0,0 +1,15 @@
+package christmas.controller;
+
+import christmas.service.BadgeService;
+
+public class BadgeController {
+    private final BadgeService badgeService;
+
+    public BadgeController() {
+        this.badgeService = new BadgeService();
+    }
+
+    public String determineBadge(int totalDiscount) {
+        return badgeService.determineEventBadge(totalDiscount);
+    }
+}
