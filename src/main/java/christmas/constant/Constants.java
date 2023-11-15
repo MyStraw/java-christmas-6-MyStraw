b77ea2687bb8daec06171c42ac4fbcdbb6dd14e4
MyStraw <kikongdosa@gmail.com>
2023-11-15 15:49:52 +0900

    refactor(Constants): 상수명 변경

--- src/main/java/christmas/constant/Constants.java
+++ src/main/java/christmas/constant/Constants.java
@@ -3,10 +3,9 @@ package christmas.constant;
 public class Constants {
     public static final int WEEKDAY_DISCOUNT_PER_DESSERT = 2_023;
     public static final int WEEKEND_DISCOUNT_PER_MAIN_DISH = 2_023;
-
     public static final int DISCOUNT_PER_DAY = 100;
     public static final int CHRISTMAS = 25;
-    public static final int SPECIAL_DISCOUNT_AMOUNT = 1_000;
+    public static final int SPECIAL_INITIAL_DISCOUNT = 1_000;
     public static final int CHAMPAGNE_PRICE = 25_000;
     public static final int MIN_ORDER_AMOUNT = 10_000;
     public static final int MAX_ITEM_COUNT = 20;
@@ -14,7 +13,6 @@ public class Constants {
     public static final int SANTA = 20_000;
     public static final int TREE = 10_000;
     public static final int STAR = 5_000;
-
 
     private Constants() { //인스턴스 생성을 애초에 막아버리기
 
