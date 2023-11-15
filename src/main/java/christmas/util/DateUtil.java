65e162cd1df60abb886507763a09bb325487daf9
MyStraw <kikongdosa@gmail.com>
2023-11-15 16:59:59 +0900

    refactor(DateUtil): 금요일과 토요일이 주말로 변경
    주말이 금요일과 토요일로 되어있다

--- src/main/java/christmas/util/DateUtil.java
+++ src/main/java/christmas/util/DateUtil.java
@@ -8,12 +8,12 @@ import java.time.LocalDate;
 public class DateUtil {
     public static boolean isWeekDay(LocalDate date) {
         DayOfWeek day = date.getDayOfWeek();
-        return !(day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY);
+        return !(day == DayOfWeek.FRIDAY ||  day == DayOfWeek.SATURDAY);
     }
 
     public static boolean isWeekEnd(LocalDate date) {
         DayOfWeek day = date.getDayOfWeek();
-        return day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY;
+        return day == DayOfWeek.FRIDAY ||  day == DayOfWeek.SATURDAY;
     }
 
     public static boolean isSpecialDay(LocalDate date) {
