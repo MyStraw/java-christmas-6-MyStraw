d2b021aac69f4305b85f57ffecf76a79f26e4b46
MyStraw <kikongdosa@gmail.com>
2023-11-15 20:02:26 +0900

    feat(BadgeService): 뱃지 판별하는 기능 추가
    -총혜택 금액에 따른 뱃지 부여

---
+++ src/main/java/christmas/service/BadgeService.java
@@ -0,0 +1,18 @@
+package christmas.service;
+
+import christmas.constant.Constants;
+
+public class BadgeService {
+
+    public static String determineEventBadge(int totalDiscount) {
+        if (totalDiscount >= Constants.SANTA) {
+            return "산타";
+        } else if (totalDiscount >= Constants.TREE) {
+            return "트리";
+        } else if (totalDiscount >= Constants.STAR) {
+            return "별";
+        } else {
+            return "없음";
+        }
+    }
+}
