b4346e1a6c10088988366d2aa29a84a5dc901e48
MyStraw <kikongdosa@gmail.com>
2023-11-15 14:13:38 +0900

    refactor(MenuTest): 메뉴 테스트 DisplayName 추가

--- src/test/java/christmas/model/MenuTest.java
+++ src/test/java/christmas/model/MenuTest.java
@@ -1,5 +1,6 @@
 package christmas.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -15,21 +16,25 @@ class MenuTest {
     }
 
     @Test
+    @DisplayName("에피타이저 메뉴 확인")
     void testAppetizer() {
         testMenuCreation("시저샐러드", 8_000);
     }
 
     @Test
+    @DisplayName("메인 메뉴 확인")
     void testMainDish() {
         testMenuCreation("해산물파스타", 35_000);
     }
 
     @Test
+    @DisplayName("디저트 메뉴 확인")
     void testDessert() {
         testMenuCreation("아이스크림", 5_000);
     }
 
     @Test
+    @DisplayName("음료 메뉴 확인")
     void testDrink() {
         testMenuCreation("레드와인", 60_000);
     }
@@ -42,21 +47,25 @@ class MenuTest {
     }
 
     @Test
+    @DisplayName("에피타이저 메뉴 개수 확인")
     void testGetAppetizers() {
         testMenuList(Menu.getAppetizer(), 3, "양송이수프");
     }
 
     @Test
+    @DisplayName("메인 메뉴 개수 확인")
     void testGetMainDishes() {
         testMenuList(Menu.getMainDishes(), 4, "티본스테이크");
     }
 
     @Test
+    @DisplayName("디저트 메뉴 개수 확인")
     void testGetDesserts() {
         testMenuList(Menu.getDesserts(), 2, "초코케이크");
     }
 
     @Test
+    @DisplayName("음료 메뉴 개수 확인")
     void testGetDrinks() {
         testMenuList(Menu.getDrinks(), 3, "제로콜라");
     }
